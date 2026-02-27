9eaf4f031e6ca1018540f01fdb31547bf905830d
Linkai Wu <linkai@linkaiwu.com>
2026-02-27 15:13:14 -0500

    feat: implement menu mode

--- project2/include/game.h
+++ project2/include/game.h
@@ -3,7 +3,11 @@
 
 class Game {
 public:
-  Game(int width, int height, const char* title);
+  Game(
+    int width,
+    int height,
+    const char* title
+  );
   ~Game();
 
   /**
@@ -12,10 +16,19 @@ public:
   void run();
 
 private:
+  enum GameState { MENU, PLAYING, GAME_OVER };
+
+  enum GameMode { ONE_PLAYER, TWO_PLAYER };
+  enum BallCount { ONE_BALL, TWO_BALLS, THREE_BALLS };
+
   int width;
   int height;
   const char* title;
   bool isRunning;
+  GameState gameState;
+
+  GameMode gameMode;
+  BallCount ballCount;
   
   void init();
 
--- project2/src/game.cpp
+++ project2/src/game.cpp
@@ -1,8 +1,12 @@
 #include "game.h"
 #include "util/log.h"
+#include "util/color.h"
 
-Game::Game(int width, int height, const char* title)
-: width(width), height(height), title(title), isRunning(true) {
+Game::Game(
+  int width,
+  int height,
+  const char* title
+) : width(width), height(height), title(title), isRunning(true), gameState(MENU), gameMode(ONE_PLAYER), ballCount(ONE_BALL) {
   init();
 }
 
@@ -30,14 +34,133 @@ void Game::processInput() {
   if (WindowShouldClose()) {
     isRunning = false;
   }
+
+  switch (gameState) {
+    case MENU: {
+      if (IsKeyPressed(KEY_T)) {
+        gameMode = gameMode == ONE_PLAYER ? TWO_PLAYER : ONE_PLAYER;
+      }
+      if (IsKeyPressed(KEY_ONE)) {
+        ballCount = ONE_BALL;
+      }
+      if (IsKeyPressed(KEY_TWO)) {
+        ballCount = TWO_BALLS;
+      }
+      if (IsKeyPressed(KEY_THREE)) {
+        ballCount = THREE_BALLS;
+      }
+      if (IsKeyPressed(KEY_SPACE)) {
+        gameState = PLAYING;
+      }
+
+      break;
+    }
+    case PLAYING: {
+      break;
+    }
+    case GAME_OVER: {
+      break;
+    }
+  }
 }
 
-void Game::update(float deltaTime) {}
+void Game::update(float deltaTime) {
+  switch (gameState) {
+    case MENU: {
+      break;
+    }
+    case PLAYING: {
+      break;
+    }
+      break;
+    case GAME_OVER: {
+      break;
+    }
+  }
+}
 
 void Game::render() {
   BeginDrawing();
 
-  ClearBackground(RAYWHITE);
+  ClearBackground(ColorFromHex("#000000"));
+
+  switch (gameState) {
+    case MENU: {
+      const int headingFontSize = 40;
+      const int paragraphFontSize = 20;
+
+      // Title
+      const char* title = "Gravity Pong";
+      const int titleWidth = MeasureText("Gravity Pong", headingFontSize);
+      DrawText(
+        "Gravity Pong",
+        width/2 - titleWidth/2,
+        height/2 - headingFontSize - 90,
+        headingFontSize,
+        ColorFromHex("#FFFFFF")
+      );
+
+      // Game Mode selector
+      const char* modeTitle = "Game Mode (T)";  
+      const int modeTitleWidth = MeasureText(modeTitle, paragraphFontSize);
+      DrawText(
+        modeTitle,
+        width/2 - 175,
+        height/2 - paragraphFontSize/2 - 25,
+        paragraphFontSize,
+        ColorFromHex("#FFFFFF")
+      );
+
+      const char* modeValue = gameMode == ONE_PLAYER ? "1 Player" : "2 Player";
+      const int modeValueWidth = MeasureText(modeValue, paragraphFontSize);
+      DrawText(
+        modeValue,
+        width/2 - modeValueWidth + 175,
+        height/2 - paragraphFontSize/2 - 25,
+        paragraphFontSize,
+        ColorFromHex("#888888")
+      );
+
+      // Ball Count selector
+      const char* ballCountTitle = "Ball Count (1-3)";  
+      const int ballCountTitleWidth = MeasureText(ballCountTitle, paragraphFontSize);
+      DrawText(
+        ballCountTitle,
+        width/2 - 175,
+        height/2 - paragraphFontSize/2 + 25,
+        paragraphFontSize,
+        ColorFromHex("#FFFFFF")
+      );  
+
+      const char* ballCountValue = ballCount == ONE_BALL ? "1x" : ballCount == TWO_BALLS ? "2x" : "3x";
+      const int ballCountValueWidth = MeasureText(ballCountValue, paragraphFontSize);
+      DrawText(
+        ballCountValue,
+        width/2 - ballCountValueWidth + 175,
+        height/2 - paragraphFontSize/2 + 25,
+        paragraphFontSize,
+        ColorFromHex("#888888")
+      );
+
+      // Start hint
+      const char* startHint = "Press SPACE to start";
+      const int startHintWidth = MeasureText(startHint, paragraphFontSize);
+      DrawText(
+        startHint,
+        width/2 - startHintWidth/2,
+        height/2 + 90,
+        paragraphFontSize,
+        ColorFromHex("#888888")
+      );
+      break;
+    }
+    case PLAYING: {
+      break;
+    }
+    case GAME_OVER: {
+      break;
+    }
+  }
 
   EndDrawing();
 }
