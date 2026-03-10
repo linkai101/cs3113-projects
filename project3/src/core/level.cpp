6dae6d1e7cda72d288f2e38f796f09ee900f5479
Linkai Wu <linkai@linkaiwu.com>
2026-03-10 18:38:57 -0400

    feat: update level tile design

--- project3/src/core/level.cpp
+++ project3/src/core/level.cpp
@@ -25,27 +25,80 @@ Level::Level(
   createBackgroundLayer(mountains1Texture);
   createBackgroundLayer(mountains2Texture);
 
-  // Create left wall
-  Entity leftWall = Entity(Vector2{ 0, -1000 });
-  leftWall.enablePhysics(Vector2{ 10, static_cast<float>(screenHeight) + 1000 }, Vector2{ -10, 0 }, true);
-  collidableEntities.push_back(std::make_unique<Entity>(leftWall));
-
   // Create tiles
   createTile({ 0, 0 }, TileType::RIGHT);
   createTile({ 0, 1 }, TileType::RIGHT);
   createTile({ 0, 2 }, TileType::CORNER_BOTTOM_RIGHT);
-  createTile({ 1, 2 }, TileType::BOTTOM);
-  createTile({ 2, 2 }, TileType::BOTTOM);
-  createTile({ 3, 2 }, TileType::BOTTOM);
-  createTile({ 4, 2 }, TileType::BOTTOM_RIGHT);
   createTile({ 0, 3 }, TileType::TOP);
-  createTile({ 1, 3 }, TileType::TOP);
-  createTile({ 2, 3 }, TileType::TOP_ALT);
-  createTile({ 3, 3 }, TileType::TOP);
-  createTile({ 4, 3 }, TileType::TOP_RIGHT);
   createTile({ 1, 1 }, TileType::SLOPE_TOP_LEFT, false);
+  createTile({ 1, 2 }, TileType::BOTTOM);
+  createTile({ 1, 3 }, TileType::TOP);
+  createTile({ 2, 2 }, TileType::BOTTOM);
+  createTile({ 2, 3 }, TileType::TOP_ALT);
+  createTile({ 3, 2 }, TileType::BOTTOM);
+  createTile({ 3, 3 }, TileType::TOP);
+  createTile({ 4, 2 }, TileType::BOTTOM_RIGHT);
+  createTile({ 4, 3 }, TileType::TOP_RIGHT);
 
-  createTile({ 7, 3 }, TileType::BLOCK);
+  createTile({ 7, 2 }, TileType::BLOCK);
+  createTile({ 8, 2 }, TileType::BLOCK);
+
+  createTile({ 11, 1 }, TileType::SLOPE_TOP_RIGHT, false);
+  createTile({ 11, 2 }, TileType::TOP_LEFT);
+  createTile({ 12, 0 }, TileType::LEFT);
+  createTile({ 12, 1 }, TileType::CORNER_BOTTOM_LEFT);
+  createTile({ 12, 2 }, TileType::CORNER_TOP_LEFT);
+  createTile({ 12, 3 }, TileType::LEFT);
+  createTile({ 12, 4 }, TileType::LEFT);
+  createTile({ 12, 5 }, TileType::TOP_LEFT);
+  createTile({ 13, 0 }, TileType::INNER);
+  createTile({ 13, 1 }, TileType::INNER);
+  createTile({ 13, 2 }, TileType::INNER);
+  createTile({ 13, 3 }, TileType::INNER);
+  createTile({ 13, 4 }, TileType::INNER);
+  createTile({ 13, 5 }, TileType::TOP);
+  createTile({ 14, 0 }, TileType::INNER);
+  createTile({ 14, 1 }, TileType::INNER);
+  createTile({ 14, 2 }, TileType::INNER);
+  createTile({ 14, 3 }, TileType::INNER);
+  createTile({ 14, 4 }, TileType::INNER_FOSSIL);
+  createTile({ 14, 5 }, TileType::TOP);
+  createTile({ 15, 0 }, TileType::INNER);
+  createTile({ 15, 1 }, TileType::INNER);
+  createTile({ 15, 2 }, TileType::INNER);
+  createTile({ 15, 3 }, TileType::INNER);
+  createTile({ 15, 4 }, TileType::INNER);
+  createTile({ 15, 5 }, TileType::TOP);
+  createTile({ 15, 0 }, TileType::INNER);
+  createTile({ 16, 1 }, TileType::INNER);
+  createTile({ 16, 2 }, TileType::CORNER_TOP_RIGHT);
+  createTile({ 16, 3 }, TileType::RIGHT);
+  createTile({ 16, 4 }, TileType::RIGHT);
+  createTile({ 16, 5 }, TileType::TOP_RIGHT);
+  createTile({ 17, 0 }, TileType::INNER);
+  createTile({ 17, 1 }, TileType::INNER);
+  createTile({ 17, 2 }, TileType::TOP);
+  createTile({ 18, 0 }, TileType::INNER);
+  createTile({ 18, 1 }, TileType::INNER);
+  createTile({ 18, 2 }, TileType::TOP);
+  createTile({ 19, 0 }, TileType::INNER);
+  createTile({ 19, 1 }, TileType::INNER);
+  createTile({ 19, 2 }, TileType::TOP);
+  createTile({ 20, 0 }, TileType::INNER);
+  createTile({ 20, 1 }, TileType::INNER);
+  createTile({ 20, 2 }, TileType::TOP);
+  createTile({ 21, 0 }, TileType::INNER);
+  createTile({ 21, 1 }, TileType::INNER);
+  createTile({ 21, 2 }, TileType::TOP);
+  createTile({ 21, 0 }, TileType::INNER);
+  createTile({ 22, 1 }, TileType::INNER);
+  createTile({ 22, 2 }, TileType::TOP);
+  createTile({ 23, 0 }, TileType::INNER);
+  createTile({ 23, 1 }, TileType::INNER);
+  createTile({ 23, 2 }, TileType::TOP);
+  createTile({ 24, 0 }, TileType::INNER);
+  createTile({ 24, 1 }, TileType::INNER);
+  createTile({ 24, 2 }, TileType::TOP);
 
   // Create player
   Animator playerAnimator = Animator(
@@ -56,7 +109,7 @@ Level::Level(
   playerAnimator.addAnimation("standing_idle", Animator::Animation{"standing_idle", { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, 10, true});
   playerAnimator.addAnimation("standing_walk", Animator::Animation{"standing_walk", { 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 }, 20, true});
   playerAnimator.addAnimation("standing_run", Animator::Animation{"standing_run", { 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44 }, 30, true});
-  playerAnimator.addAnimation("standing_jump", Animator::Animation{"standing_jump", { 45, 46, 47, 48, 49, 50, 51, 52, 53, 54 }, 20, false});
+  playerAnimator.addAnimation("standing_jump", Animator::Animation{"standing_jump", { 45, 46, 47, 48, 49, 50, 51, 52, 53, 54 }, 15, false});
   playerAnimator.addAnimation("standing_die", Animator::Animation{"standing_die", { 55, 56, 57, 58, 59 }, 15, false});
   playerAnimator.addAnimation("flying", Animator::Animation{"flying", { 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74 }, 20, true});
   playerAnimator.addAnimation("flying_no_movement", Animator::Animation{"flying_no_movement", { 75, 76, 77, 78, 79, 80, 81, 82, 83, 84 }, 20, true});
@@ -122,7 +175,14 @@ void Level::resolveCollisions() {
   if (!player || !player->physicsBody) return;
   PhysicsBody& pb = *player->physicsBody;
 
-  // Check if player is out of bounds
+  // Invisible left wall collision
+  Rectangle playerBox = pb.getCollider(player->position);
+  if (playerBox.x < 0) {
+    player->position.x -= playerBox.x;
+    pb.velocity.x = 0;
+  }
+
+  // TEMP: respawn player if they fall off the screen
   if (player->position.y - pb.getCollider(player->position).height > static_cast<float>(screenHeight) + 1000) {
     player->position = getPositionFromTileCoordinates({ 2, 5 }, screenWidth, screenHeight);
     pb.velocity = { 0, 0 };
