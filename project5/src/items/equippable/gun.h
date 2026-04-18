ca602879e07a6a32285420f62e10e2236d38d970
Linkai Wu <linkai@linkaiwu.com>
2026-04-18 15:42:27 -0400

    fix: buff shotgun damage

--- project5/src/items/equippable/gun.h
+++ project5/src/items/equippable/gun.h
@@ -64,5 +64,5 @@ private:
 
   static constexpr Properties RIFLE_PROPERTIES   = {0.15f, 700.0f, 900.0f, 25.0f, 1, 0.0f, 30, false};
   static constexpr Properties PISTOL_PROPERTIES  = {0.25f, 450.0f, 500.0f, 20.0f, 1, 0.0f, 12, false};
-  static constexpr Properties SHOTGUN_PROPERTIES = {0.6f,  350.0f, 300.0f, 15.0f, 3, 0.2f,  6, true};
+  static constexpr Properties SHOTGUN_PROPERTIES = {0.6f,  350.0f, 300.0f, 17.0f, 3, 0.2f,  6, true};
 };
